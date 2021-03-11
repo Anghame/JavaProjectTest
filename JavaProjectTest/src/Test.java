ba0338333050f9af1d4ddb22481da8d0d51ede18
ZineddineBK-DEV <55902255+ZineddineBK-DEV@users.noreply.github.com>
2021-03-11 23:53:37 +0100

    Update Test.java

--- JavaProjectTest/src/Test.java
+++ JavaProjectTest/src/Test.java
@@ -4,6 +4,9 @@ public class Test {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		// ya anghame chniya hedha ??
+		kbdksbqlksqkldjlsqkbckjsqbckj
+			dmnfckjbkjcsqljcnjls
+			djbckjsbqkjcbkjs
 	}
 
 }
